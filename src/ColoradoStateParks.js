a698e430b7ea12a8bd2c78dc92285dd7d3fcbaed
LunarSalameh <lun20190983@std.psut.edu.jo>
2023-10-17 20:41:04 +0300

    Finished the Assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,16 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import {trees,wildlife} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
-  howManyParks(); // => "42 parks!"
+  return (
+    <div>
+    console.log(trees);
+    wildlife();
 
-  return <h1>Colorado State Parks!</h1>;
+      </div>
+  );
 }
+
+export default ColoradoStateParks;
